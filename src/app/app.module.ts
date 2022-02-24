2b76d236e61b5c5e3b8577ae364a2486f8e29b2b
michael-muga <mugamichaelshawn@gmail.com>
2022-02-24 17:48:19 +0300

    added imports and ng modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GitsearchFormComponent } from './gitsearch-form/gitsearch-form/gitsearch-form.component';
+import { NavComponent } from './nav/nav/nav.component';
+import { NotfoundComponent } from './notfound/notfound/notfound.component';
+import { RepoComponent } from './repo/repo/repo.component';
+import { SearchGitComponent } from './search-git/search-git/search-git.component';
+import { HighlightDirective } from './highlight.directive';
+import { TimeAgoPipe } from './time-ago.pipe';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GitsearchFormComponent,
+    NavComponent,
+    NotfoundComponent,
+    RepoComponent,
+    SearchGitComponent,
+    HighlightDirective,
+    TimeAgoPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
