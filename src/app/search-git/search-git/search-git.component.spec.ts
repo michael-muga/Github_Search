56011520ea6df8cf95a5cf7c8a6ff4cade0e3274
michael-muga <mugamichaelshawn@gmail.com>
2022-02-24 17:41:39 +0300

    initial commit

---
+++ src/app/search-git/search-git/search-git.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SearchGitComponent } from './search-git.component';
+
+describe('SearchGitComponent', () => {
+  let component: SearchGitComponent;
+  let fixture: ComponentFixture<SearchGitComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SearchGitComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SearchGitComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
