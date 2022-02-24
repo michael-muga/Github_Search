284e8a83a2ed87c48b2e132f46411aa8ff7ac409
michael-muga <mugamichaelshawn@gmail.com>
2022-02-24 17:43:42 +0300

    other module imports, added array of paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RepoComponent } from './repo/repo/repo.component';
+import { SearchGitComponent } from './search-git/search-git/search-git.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"Home", component: SearchGitComponent
+  },
+  {
+    path:"repo", component: RepoComponent,
+  },
+  //  { path:'**', component:NotfoundComponent},
+  { path: '', redirectTo:"/Home", pathMatch:"full"},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
