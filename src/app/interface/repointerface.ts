0b007dd9aba0cd7f604dee6d50720e3f8d8a9141
michael-muga <mugamichaelshawn@gmail.com>
2022-02-24 17:19:36 +0300

    interface functionalities added for both the user and repo classes

---
+++ src/app/interface/repointerface.ts
@@ -0,0 +1,10 @@
+export interface Interrepo {
+    name: string;
+    html_url: string;
+    description: string;
+    created_at: string;
+    clone_url: string;
+    homepage: string;
+    forks_count: number;
+    stargazers_count: number;
+}
---
+++ src/app/interface/userinterface.ts
@@ -0,0 +1,13 @@
+export interface Interuser {
+    login: string;
+    name: any;
+    avatar_url: string;
+    html_url: string;
+    repos_url: string;
+    bio: string;
+    followers: number;
+    following: number;
+    twitter_username: string;
+    public_repos: number;
+    location: string;
+}
